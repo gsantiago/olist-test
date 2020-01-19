d77ba176d502b2b13b5a8f18be7a26777ce49cef
Guilherme Santiago <guilherme.santiago95@gmail.com>
2020-01-19 18:50:15 -0300

    setup storybook and add logo component

---
+++ src/components/Logo.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Logo () {
+  return (
+    <img
+      src={require('../images/logo.svg')}
+      width={88}
+      height={35}
+      alt='Olist logo'
+    />
+  )
+}
---
+++ src/stories/1-Logo.stories.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import Logo from '../components/Logo'
+
+export default {
+  title: 'Logo',
+  component: Logo
+}
+
+export const Example = () => <Logo />
